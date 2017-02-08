ccca625d526cdb60a60462ff3fd546a6ef125db1
Andrew Ross <andrew@vermonster.com>
2017-02-08 15:29:02 -0500

    update NASM script to account for website changes

--- nasm.rb
+++ nasm.rb
@@ -4,9 +4,19 @@ require 'csv'
 
 class NasmParser < Mechanize
   HOME = 'http://nasm.arts-accredit.org/'
-  SCHOOL = /<h2>(.*)<\/h2>/
-  SITE = /Web Site: .*">(.*)<\/a>/
-  CONTACT = /<br>(.*) (.*), <i>(.*)<\/i>.*E-Mail *(.*) *<br>/
+  SCHOOL = 'div.wpb_wrapper h2'
+  SITE = 'div.wpb_wrapper h2 + p > a'
+
+  def scan_contacts(page, prefix='')
+    info = page.search("h3:contains('Contacts')#{prefix} + p").inner_html
+    if info.empty?
+      []
+    else
+      name, title, department = info.split('<br>')[0].split(',').map{|s| s.strip.squeeze}
+      email = info[/E-Mail: <[^>]+>([^<]+)<\/a>/, 1]
+      [[name, email, title, department]] + scan_contacts(page, "#{prefix} + p")
+    end
+  end
 
   def process(user, password)
     user_agent_alias = 'Mac Safari'
@@ -14,28 +24,27 @@ class NasmParser < Mechanize
 
     # login
     click page.link_with(:text => /login/i)
-    page.form_with(:action => /login/i) do |f|
-      f.login = user
+    page.form_with(:class => /login/i) do |f|
+      f.username = user
       f.password = password
       f.click_button
     end
 
     # list members
-    click page.link_with(:text => /directory/i)
-    click page.link_with(:text => 'ACCREDITED INSTITUTIONAL MEMBERS')
-    page.form_with(:action => /List_Accredited_Members/).click_button
+    click page.link_with(:text => /advanced search/i)
+    page.form_with(:id => 'institution-search').click_button
 
     # fetch and process one member
-    l = page.links_with(:href => /memberid/i)
+    l = page.links_with(:href => /\?id=/i)
+
     l.each do |link|
       transact do
         click link
-        data = page.search('td.maintxt td.maintxt').inner_html
-        school = data.scan(SCHOOL).flatten.first
-        site = data.scan(SITE).flatten.first
-        contacts = data.scan(CONTACT)
+        school = page.search(SCHOOL).inner_html
+        site = page.search(SITE).inner_html
+        contacts = scan_contacts(page)
         if contacts.empty?
-          yield [school, site, '', '', '', '']
+          yield [school, site, '', '', '', '', '']
         else
           contacts.each do |contact|
             yield [school, site] + contact
@@ -44,7 +53,6 @@ class NasmParser < Mechanize
       end
     end
 
-    click page.link_with(:text => /logout/i)
     l.length
   end
 end
